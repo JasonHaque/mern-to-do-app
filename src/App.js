93d87598a01d268fc3b1956d6b3abe13788325dc
jason <zahinjason220434@gmail.com>
2019-09-13 11:53:46 +0600

    Added basic routing

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import {BrowserRouter as Router,Route,Link} from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
-
+import CreateTodo from "./components/create-todo.component";
+import EditTodo from "./components/edit-todo.component";
+import TodosList from "./components/todos-list.component";
 function App() {
   return (
     <Router>
       <div className="container">
         <h2>MERN-Stack to-do app</h2>
+        <Route path="/" exact component={TodosList}/>
+        <Route path="/edit/:id" component={EditTodo} />
+        <Route path="/create" component={CreateTodo} />
       </div>
-      <Route path="/" exact component={TodosList}/>
-      <Route path="/edit/:id" component={EditTodo} />
-      <Route path="create" component={CreateTodo} />
+      
     </Router>
   );
 }

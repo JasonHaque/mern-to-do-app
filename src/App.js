552ba9850067e1285fbf413c4c6185a7233c5638
jason <zahinjason220434@gmail.com>
2019-09-13 14:04:06 +0600

    Added navbar

--- src/App.js
+++ src/App.js
@@ -8,7 +8,19 @@ function App() {
   return (
     <Router>
       <div className="container">
-        <h2>MERN-Stack to-do app</h2>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <Link to="/" className="navbar-brand">MERN-Stack Todo App</Link>
+      <div className="collpase navbar-collapse">
+              <ul className="navbar-nav mr-auto">
+                <li className="navbar-item">
+                  <Link to="/" className="nav-link">Todos</Link>
+                </li>
+                <li className="navbar-item">
+                  <Link to="/create" className="nav-link">Create Todo</Link>
+                </li>
+              </ul>
+            </div>
+      </nav>
         <Route path="/" exact component={TodosList}/>
         <Route path="/edit/:id" component={EditTodo} />
         <Route path="/create" component={CreateTodo} />
